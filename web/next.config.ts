692028eb9f3410489fa18ac312e29fad0aa039ff
tonycho999 <tonycho999@gmail.com>
2026-02-11 13:26:16 +0000

    Update: Apply Space Theme & Real API News

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['placehold.co', 'i.namu.wiki', 'wallpapercave.com', 'ssl.pstatic.net', 'imgnews.pstatic.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
